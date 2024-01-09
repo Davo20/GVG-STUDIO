3ada1383e65bf0d88bd78f0d7595392599cee4a4
Davo20 <dav507805@gmail.com>
2024-01-09 18:11:04 +0400

    GVG Studio

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -13,8 +13,8 @@ export default function Header({ selectLanguage, language }) {
         },
         tablet: {
             breakpoint: { max: 1024, min: 464 },
-            items: 2,
-            slidesToSlide: 2 // optional, default to 1.
+            items: 1,
+            slidesToSlide: 1 // optional, default to 1.
         },
         mobile: {
             breakpoint: { max: 464, min: 0 },
